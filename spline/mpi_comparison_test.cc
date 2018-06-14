0197552e68c5d196a48e8c50433fd070746f14f8
Robin Richardson <robin.richardson@ucl.ac.uk>
2018-06-14 15:08:17 +0100

    mpi_comparison_test now reads in its strain histories from a results folder

--- spline/mpi_comparison_test.cc
+++ spline/mpi_comparison_test.cc
@@ -9,14 +9,51 @@
 #include <iostream>
 #include <limits.h>
 #include <assert.h>
+#include <sys/types.h>
+#include <dirent.h>
+
 
 #include <mpi.h>
 
 #include "strain2spline.h"
 
+bool starts_with(std::string mainStr, std::string toMatch)
+{
+	// std::string::find returns 0 if toMatch is found at starting
+	if(mainStr.find(toMatch) == 0)
+		return true;
+	else
+		return false;
+}
+
+void read_directory(const std::string& name, std::vector<std::string>& v)
+{
+	DIR* dirp = opendir(name.c_str());
+	struct dirent * dp;
+	while ((dp = readdir(dirp)) != NULL) {
+		v.push_back(dp->d_name);
+	}
+	closedir(dirp);
+}
+
+void erase_substring(std::string & mainStr, const std::string &toErase)
+{
+	size_t pos = mainStr.find(toErase);
+	if (pos != std::string::npos) {
+		mainStr.erase(pos, toErase.length());
+	}
+}
+
 int main(int argc, char **argv)
 {
-	const double acceptable_diff_threshold = 100.0; 
+	if(argc != 4) {
+		fprintf(stderr, "Usage: ./mpi_comparison_test STRAIN_DIRECTORY NUM_SPLINE_POINTS THRESH\n");
+		return 1;
+	}
+
+	char *straindir = argv[1];
+	uint32_t num_points = atoi(argv[2]);
+	double acceptable_diff_threshold = atof(argv[3]); 
 
 	MPI_Comm comm = MPI_COMM_WORLD;
 
@@ -27,39 +64,49 @@ int main(int argc, char **argv)
 	MPI_Comm_rank(comm, &this_rank);
 	MPI_Comm_size(comm, &num_ranks);
 
-	// Build some Strain6D objects for this rank
-	uint32_t num_histories_on_this_rank = 1;
-	std::vector<MatHistPredict::Strain6D*> histories;
-	std::srand(this_rank*100); // never use srand(0) or srand(1) - they give the same...
-	std::cout << "Rank " << this_rank << ": " << std::rand() << "\n";
-	for(uint32_t i = 0; i < num_histories_on_this_rank; i++) {
-		MatHistPredict::Strain6D *new_s6D = new MatHistPredict::Strain6D();
+	// Get list of strain history files in given directory
+	std::vector<std::string> fnames;
+	read_directory(straindir, fnames);
 
-		for(uint32_t j = 0; j < 5; j++) {
-			new_s6D->add_current_strain(	std::rand()/(float)RAND_MAX,
-							std::rand()/(float)RAND_MAX,
-							std::rand()/(float)RAND_MAX,
-							std::rand()/(float)RAND_MAX,
-							std::rand()/(float)RAND_MAX,
-							std::rand()/(float)RAND_MAX);
+	// Read all strain histories to vector
+	//time_t start_read = time(NULL);
+	std::vector<MatHistPredict::Strain6D*> histories;
+	int32_t i = 0;
+	for(std::string fname : fnames) {
+		if(!starts_with(fname, "strain_")) {
+			std::cout << "Ignoring: '" << fname << "'\n";
+			continue;
 		}
-		new_s6D->splinify(10);
-		new_s6D->set_ID(this_rank * 10 + i);
-		std::cout << "Rank " << this_rank << ":\n";
-		new_s6D->print();
-		histories.push_back(new_s6D);
+
+		if(i % num_ranks == this_rank) {
+			MatHistPredict::Strain6D *new_s6D = new MatHistPredict::Strain6D();
+			new_s6D->from_file((std::string(straindir) + fname).c_str());
+
+			new_s6D->splinify(num_points);
+
+			erase_substring(fname, "strain_");
+			uint32_t id = atoi(fname.c_str());
+//			std::cout << id << "\n";
+			new_s6D->set_ID(id);
+
+			histories.push_back(new_s6D);
+		}
+		i++;
 	}
+	uint32_t num_histories_on_this_rank = histories.size();
+	//time_t end_read = time(NULL);
+
 	
 	// Find the most similar strain histories
 	MatHistPredict::compare_histories_with_all_ranks(histories, acceptable_diff_threshold, comm);
 
 	// Results
 	for(uint32_t i=0; i < num_histories_on_this_rank; i++) {
-		bool will_run_new_MD = histories[i]->run_new_sim(acceptable_diff_threshold);
-		std::cout << "Rank " << this_rank << ": Hist" << histories[i]->get_ID() << " is most similar to Hist" << histories[i]->get_most_similar_history_ID() << " with diff " << histories[i]->get_most_similar_history_diff() << " => will_run_new_MD=" << will_run_new_MD << "\n";
+		//bool will_run_new_MD = histories[i]->run_new_sim(acceptable_diff_threshold);
+//		std::cout << "Rank " << this_rank << ": Hist" << histories[i]->get_ID() << " is most similar to Hist" << histories[i]->get_most_similar_history_ID() << " with diff " << histories[i]->get_most_similar_history_diff() << " => will_run_new_MD=" << will_run_new_MD << "\n";
 
-		histories[i]->print_most_similar_histories();
-		histories[i]->most_similar_histories_to_file(("Rank_" + std::to_string(this_rank) + ".txt").c_str());
+		//histories[i]->print_most_similar_histories();
+		histories[i]->most_similar_histories_to_file(("__results/ID_" + std::to_string(histories[i]->get_ID()) + ".txt").c_str());
 	}
 
 	MPI_Finalize();
--- spline/render_network.py
+++ spline/render_network.py
@@ -1,5 +1,14 @@
+import os
 import networkx as nx
+import operator
 import matplotlib.pyplot as plt
+
+def get_max_degree_node(x):
+	sorted_x = sorted(x.items(), key=operator.itemgetter(1))
+	return sorted_x[-1][0]
+
+
+os.system("cat __results/ID_* > network.txt\n")
 
 G = nx.Graph()
 
@@ -10,14 +19,36 @@ with open("network.txt") as infile:
 		cell1 = int(cell1)
 		cell2 = int(cell2)
 		dist = float(dist)
-		print cell1, cell2, dist
+#		print cell1, cell2, dist
 
-		G.add_edge(cell1, cell2, weight=dist)
+#		G.add_edge(cell1, cell2)
+		G.add_edge(cell1, cell2, weight=1.0/dist)
 		labels[cell1] = str(cell1)
 		labels[cell2] = str(cell2)
 
+#print(G.degree())
+#rofl = sorted(set(G.degree().values()))
+#rofl = sorted(G.degree, key=lambda x: x[1], reverse=True)
+
+num_nodes_remaining = len(G)
+
+while num_nodes_remaining > 0:
+	node = get_max_degree_node(G.degree())
+	print "Max degree node = ", node
+
+	neighbours = [node]
+	for neighbour in nx.all_neighbors(G, node):
+		neighbours.append(neighbour)
+	print "Neighbours:", neighbours
+
+	print "Removing", len(neighbours)
+	G.remove_nodes_from(neighbours)
+
+	num_nodes_remaining = len(G)
+	print "Num nodes remaining", len(G)
+
 pos=nx.spring_layout(G)
-nx.draw_networkx_nodes(G, pos, alpha=0.5)
+nx.draw_networkx_nodes(G, pos, alpha=0.5, node_size=1)
 nx.draw_networkx_edges(G, pos)
-nx.draw_networkx_labels(G, pos, labels, font_size=10)
+#nx.draw_networkx_labels(G, pos, labels, font_size=10)
 plt.show()

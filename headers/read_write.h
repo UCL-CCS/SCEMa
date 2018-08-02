bbaa06c69b7efec4f3b2b8d2054df260fecd35b8
Maxime Vassaux <m.vassaux@ucl.ac.uk>
2018-08-03 00:45:50 +0100

    adding headers for generic functions, might be a good idea to compile and link them

---
+++ headers/read_write.h
@@ -0,0 +1,245 @@
+#ifndef READ_WRITE_H
+#define READ_WRITE_H
+
+#include "boost/property_tree/ptree.hpp"
+
+using namespace dealii;
+
+void bptree_print(boost::property_tree::ptree const& pt)
+{
+	using boost::property_tree::ptree;
+	ptree::const_iterator end = pt.end();
+	for (ptree::const_iterator it = pt.begin(); it != end; ++it) {
+		std::cout << it->first << ": " << it->second.get_value<std::string>() << std::endl;
+		bptree_print(it->second);
+	}
+}
+
+std::string bptree_read(boost::property_tree::ptree const& pt, std::string key)
+{
+	std::string value = "NULL";
+	using boost::property_tree::ptree;
+	ptree::const_iterator end = pt.end();
+	for (ptree::const_iterator it = pt.begin(); it != end; ++it) {
+		if(it->first==key)
+			value = it->second.get_value<std::string>();
+	}
+	return value;
+}
+
+std::string bptree_read(boost::property_tree::ptree const& pt, std::string key1, std::string key2)
+{
+	std::string value = "NULL";
+	using boost::property_tree::ptree;
+	ptree::const_iterator end = pt.end();
+	for (ptree::const_iterator it = pt.begin(); it != end; ++it) {
+		if(it->first==key1){
+			value = bptree_read(it->second, key2);
+		}
+	}
+	return value;
+}
+
+std::string bptree_read(boost::property_tree::ptree const& pt, std::string key1, std::string key2, std::string key3)
+{
+	std::string value = "NULL";
+	using boost::property_tree::ptree;
+	ptree::const_iterator end = pt.end();
+	for (ptree::const_iterator it = pt.begin(); it != end; ++it) {
+		if(it->first==key1){
+			value = bptree_read(it->second, key2, key3);
+		}
+	}
+	return value;
+}
+
+boost::property_tree::ptree get_subbptree(boost::property_tree::ptree const& pt, std::string key1)
+{
+	boost::property_tree::ptree value;
+	using boost::property_tree::ptree;
+	ptree::const_iterator end = pt.end();
+	for (ptree::const_iterator it = pt.begin(); it != end; ++it) {
+		if(it->first==key1){
+			value = it->second;
+		}
+	}
+	return value;
+}
+
+
+bool file_exists(std::string file) {
+	struct stat buf;
+	return (stat(file.c_str(), &buf) == 0);
+}
+
+
+bool file_exists(const char* file) {
+	struct stat buf;
+	return (stat(file, &buf) == 0);
+}
+
+
+template <int dim>
+inline
+void
+read_tensor (const char *filename, double &tensor)
+{
+	std::ifstream ifile;
+
+	ifile.open (filename);
+	if (ifile.is_open())
+	{
+		char line[1024];
+		if(ifile.getline(line, sizeof(line)))
+			tensor = std::strtod(line, NULL);
+		ifile.close();
+	}
+	else std::cout << "Unable to open" << filename << " to read it" << std::endl;
+}
+
+template <int dim>
+inline
+void
+read_tensor (const char *filename, Tensor<1,dim> &tensor)
+{
+	std::ifstream ifile;
+
+	ifile.open (filename);
+	if (ifile.is_open())
+	{
+		for(unsigned int k=0;k<dim;k++)
+			{
+				char line[1024];
+				if(ifile.getline(line, sizeof(line)))
+					tensor[k] = std::strtod(line, NULL);
+			}
+		ifile.close();
+	}
+	else std::cout << "Unable to open" << filename << " to read it" << std::endl;
+}
+
+template <int dim>
+inline
+bool
+read_tensor (const char *filename, SymmetricTensor<2,dim> &tensor)
+{
+	std::ifstream ifile;
+
+	bool load_ok = false;
+
+	ifile.open (filename);
+	if (ifile.is_open())
+	{
+		load_ok = true;
+		for(unsigned int k=0;k<dim;k++)
+			for(unsigned int l=k;l<dim;l++)
+			{
+				char line[1024];
+				if(ifile.getline(line, sizeof(line)))
+					tensor[k][l] = std::strtod(line, NULL);
+			}
+		ifile.close();
+	}
+	else std::cout << "Unable to open" << filename << " to read it" << std::endl;
+return load_ok;
+}
+
+template <int dim>
+inline
+void
+read_tensor (const char *filename, SymmetricTensor<4,dim> &tensor)
+{
+	std::ifstream ifile;
+
+	ifile.open (filename);
+	if (ifile.is_open())
+	{
+		for(unsigned int k=0;k<dim;k++)
+			for(unsigned int l=k;l<dim;l++)
+				for(unsigned int m=0;m<dim;m++)
+					for(unsigned int n=m;n<dim;n++)
+					{
+						char line[1024];
+						if(ifile.getline(line, sizeof(line)))
+							tensor[k][l][m][n]= std::strtod(line, NULL);
+					}
+		ifile.close();
+	}
+	else std::cout << "Unable to open" << filename << " to read it..." << std::endl;
+}
+
+template <int dim>
+inline
+void
+write_tensor (const char *filename, double &tensor)
+{
+	std::ofstream ofile;
+
+	ofile.open (filename);
+	if (ofile.is_open())
+	{
+		ofile << std::setprecision(16) << tensor << std::endl;
+		ofile.close();
+	}
+	else std::cout << "Unable to open" << filename << " to write in it" << std::endl;
+}
+
+template <int dim>
+inline
+void
+write_tensor (const char *filename, Tensor<1,dim> &tensor)
+{
+	std::ofstream ofile;
+
+	ofile.open (filename);
+	if (ofile.is_open())
+	{
+		for(unsigned int k=0;k<dim;k++)
+				//std::cout << std::setprecision(16) << tensor[k][l] << std::endl;
+				ofile << std::setprecision(16) << tensor[k] << std::endl;
+		ofile.close();
+	}
+	else std::cout << "Unable to open" << filename << " to write in it" << std::endl;
+}
+
+template <int dim>
+inline
+void
+write_tensor (const char *filename, SymmetricTensor<2,dim> &tensor)
+{
+	std::ofstream ofile;
+
+	ofile.open (filename);
+	if (ofile.is_open())
+	{
+		for(unsigned int k=0;k<dim;k++)
+			for(unsigned int l=k;l<dim;l++)
+				//std::cout << std::setprecision(16) << tensor[k][l] << std::endl;
+				ofile << std::setprecision(16) << tensor[k][l] << std::endl;
+		ofile.close();
+	}
+	else std::cout << "Unable to open" << filename << " to write in it" << std::endl;
+}
+
+template <int dim>
+inline
+void
+write_tensor (const char *filename, SymmetricTensor<4,dim> &tensor)
+{
+	std::ofstream ofile;
+
+	ofile.open (filename);
+	if (ofile.is_open())
+	{
+		for(unsigned int k=0;k<dim;k++)
+			for(unsigned int l=k;l<dim;l++)
+				for(unsigned int m=0;m<dim;m++)
+					for(unsigned int n=m;n<dim;n++)
+						ofile << std::setprecision(16) << tensor[k][l][m][n] << std::endl;
+		ofile.close();
+	}
+	else std::cout << "Unable to open" << filename << " to write in it" << std::endl;
+}
+
+
+#endif
---
+++ headers/tensor_calc.h
@@ -0,0 +1,181 @@
+#ifndef TENSOR_CALC_H
+#define TENSOR_CALC_H
+
+using namespace dealii;
+
+template <int dim>
+inline
+Tensor<2,dim>
+compute_rotation_tensor (Tensor<1,dim> vorig, Tensor<1,dim> vdest)
+{
+	Tensor<2,dim> rotam;
+
+	// Filling identity matrix
+	Tensor<2,dim> idmat;
+	idmat = 0.0; for (unsigned int i=0; i<dim; ++i) idmat[i][i] = 1.0;
+
+	// Decalaration variables rotation matrix computation
+	double ccos;
+	Tensor<2,dim> skew_rot;
+
+	// Compute the scalar product of the local and global vectors
+	ccos = scalar_product(vorig, vdest);
+
+	// Filling the skew-symmetric cross product matrix (a^Tb-b^Ta)
+	for (unsigned int i=0; i<dim; ++i)
+		for (unsigned int j=0; j<dim; ++j)
+			skew_rot[i][j] = vorig[j]*vdest[i] - vorig[i]*vdest[j];
+
+	// Assembling the rotation matrix
+	rotam = idmat + skew_rot + (1/(1+ccos))*skew_rot*skew_rot;
+
+	return rotam;
+}
+
+template <int dim>
+inline
+SymmetricTensor<2,dim>
+rotate_tensor (const SymmetricTensor<2,dim> &tensor,
+		const Tensor<2,dim> &rotam)
+{
+	SymmetricTensor<2,dim> stmp;
+
+	Tensor<2,dim> tmp;
+
+	Tensor<2,dim> tmp_tensor = tensor;
+
+	tmp = rotam*tmp_tensor*transpose(rotam);
+
+	for(unsigned int k=0;k<dim;k++)
+		for(unsigned int l=k;l<dim;l++)
+			stmp[k][l] = 0.5*(tmp[k][l] + tmp[l][k]);
+
+	return stmp;
+}
+
+template <int dim>
+inline
+SymmetricTensor<4,dim>
+rotate_tensor (const SymmetricTensor<4,dim> &tensor,
+		const Tensor<2,dim> &rotam)
+{
+	SymmetricTensor<4,dim> tmp;
+	tmp = 0;
+
+	// Loop over the indices of the SymmetricTensor (upper "triangle" only)
+	for(unsigned int k=0;k<dim;k++)
+		for(unsigned int l=k;l<dim;l++)
+			for(unsigned int s=0;s<dim;s++)
+				for(unsigned int t=s;t<dim;t++)
+				{
+					for(unsigned int m=0;m<dim;m++)
+						for(unsigned int n=0;n<dim;n++)
+							for(unsigned int p=0;p<dim;p++)
+								for(unsigned int r=0;r<dim;r++)
+									tmp[k][l][s][t] +=
+											tensor[m][n][p][r]
+											* rotam[k][m] * rotam[l][n]
+											* rotam[s][p] * rotam[t][r];
+				}
+
+	return tmp;
+}
+
+template <int dim>
+inline
+SymmetricTensor<2,dim>
+get_strain (const FEValues<dim> &fe_values,
+		const unsigned int   shape_func,
+		const unsigned int   q_point)
+		{
+	SymmetricTensor<2,dim> tmp;
+
+	for (unsigned int i=0; i<dim; ++i)
+		tmp[i][i] = fe_values.shape_grad_component (shape_func,q_point,i)[i];
+
+	for (unsigned int i=0; i<dim; ++i)
+		for (unsigned int j=i+1; j<dim; ++j)
+			tmp[i][j]
+				   = (fe_values.shape_grad_component (shape_func,q_point,i)[j] +
+						   fe_values.shape_grad_component (shape_func,q_point,j)[i]) / 2;
+
+	return tmp;
+		}
+
+template <int dim>
+inline
+SymmetricTensor<2,dim>
+get_strain (const std::vector<Tensor<1,dim> > &grad)
+{
+	Assert (grad.size() == dim, ExcInternalError());
+
+	SymmetricTensor<2,dim> strain;
+	for (unsigned int i=0; i<dim; ++i)
+		strain[i][i] = grad[i][i];
+
+	for (unsigned int i=0; i<dim; ++i)
+		for (unsigned int j=i+1; j<dim; ++j)
+			strain[i][j] = (grad[i][j] + grad[j][i]) / 2;
+
+	return strain;
+}
+
+
+Tensor<2,2>
+get_rotation_matrix (const std::vector<Tensor<1,2> > &grad_u)
+{
+	const double curl = (grad_u[1][0] - grad_u[0][1]);
+
+	const double angle = std::atan (curl);
+
+	const double t[2][2] = {{ cos(angle), sin(angle) },
+			{-sin(angle), cos(angle) }
+	};
+	return Tensor<2,2>(t);
+}
+
+
+Tensor<2,3>
+get_rotation_matrix (const std::vector<Tensor<1,3> > &grad_u)
+{
+	const Point<3> curl (grad_u[2][1] - grad_u[1][2],
+			grad_u[0][2] - grad_u[2][0],
+			grad_u[1][0] - grad_u[0][1]);
+
+	const double tan_angle = std::sqrt(curl*curl);
+	const double angle = std::atan (tan_angle);
+
+	if (angle < 1e-9)
+	{
+		static const double rotation[3][3]
+										= {{ 1, 0, 0}, { 0, 1, 0 }, { 0, 0, 1 } };
+		static const Tensor<2,3> rot(rotation);
+		return rot;
+	}
+
+	const double c = std::cos(angle);
+	const double s = std::sin(angle);
+	const double t = 1-c;
+
+	const Point<3> axis = curl/tan_angle;
+	const double rotation[3][3]
+							 = {{
+									 t *axis[0] *axis[0]+c,
+									 t *axis[0] *axis[1]+s *axis[2],
+									 t *axis[0] *axis[2]-s *axis[1]
+							 },
+									 {
+											 t *axis[0] *axis[1]-s *axis[2],
+											 t *axis[1] *axis[1]+c,
+											 t *axis[1] *axis[2]+s *axis[0]
+									 },
+									 {
+											 t *axis[0] *axis[2]+s *axis[1],
+											 t *axis[1] *axis[1]-s *axis[0],
+											 t *axis[2] *axis[2]+c
+									 }
+	};
+	return Tensor<2,3>(rotation);
+}
+
+#endif

e73c490daba0528abafe240e879e68db7d325976
Maxime Vassaux <m.vassaux@ucl.ac.uk>
2018-08-04 13:54:33 +0100

    tiny corrections...

--- equilammps.cc
+++ equilammps.cc
@@ -37,7 +37,6 @@
 #include "headers/read_write.h"
 #include "headers/tensor_calc.h"
 #include "headers/eqmd_sync.h"
-#include "headers/fe_problem.h"
 
 // To avoid conflicts...
 // pointers.h in input.h defines MIN and MAX
@@ -45,46 +44,8 @@
 #undef  MIN
 #undef  MAX
 
-#include <deal.II/grid/tria_boundary_lib.h>
-#include <deal.II/fe/fe_tools.h>
-#include <deal.II/fe/fe_dgq.h>
-#include <deal.II/base/quadrature_lib.h>
-#include <deal.II/base/function.h>
-#include <deal.II/base/logstream.h>
-#include <deal.II/base/multithread_info.h>
 #include <deal.II/base/conditional_ostream.h>
-#include <deal.II/base/utilities.h>
-#include <deal.II/lac/vector.h>
-#include <deal.II/lac/full_matrix.h>
-#include <deal.II/lac/dynamic_sparsity_pattern.h>
-#include <deal.II/lac/petsc_parallel_vector.h>
-#include <deal.II/lac/petsc_parallel_sparse_matrix.h>
-#include <deal.II/lac/petsc_solver.h>
-#include <deal.II/lac/petsc_precondition.h>
-#include <deal.II/lac/constraint_matrix.h>
-#include <deal.II/lac/sparsity_tools.h>
-#include <deal.II/distributed/shared_tria.h>
-#include <deal.II/grid/tria.h>
-#include <deal.II/grid/grid_generator.h>
-#include <deal.II/grid/grid_refinement.h>
-#include <deal.II/grid/tria_accessor.h>
-#include <deal.II/grid/tria_iterator.h>
-#include <deal.II/grid/manifold_lib.h>
-#include <deal.II/grid/grid_tools.h>
-#include <deal.II/grid/grid_in.h>
-#include <deal.II/dofs/dof_handler.h>
-#include <deal.II/dofs/dof_accessor.h>
-#include <deal.II/dofs/dof_tools.h>
-#include <deal.II/dofs/dof_renumbering.h>
-#include <deal.II/fe/fe_values.h>
-#include <deal.II/fe/fe_system.h>
-#include <deal.II/fe/fe_q.h>
-#include <deal.II/numerics/vector_tools.h>
-#include <deal.II/numerics/matrix_tools.h>
-#include <deal.II/numerics/data_out.h>
-#include <deal.II/numerics/error_estimator.h>
 #include <deal.II/base/symmetric_tensor.h>
-#include <deal.II/grid/filtered_iterator.h>
 #include <deal.II/base/mpi.h>
 
 namespace HMM

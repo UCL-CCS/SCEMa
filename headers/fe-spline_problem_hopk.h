cce7d55793b3612160539ebacb73d07082fd0d40
Mr Robert Sinclair <rcsinclair3@gmail.com>
2019-02-08 17:24:20 +0000

    remove testing messages

--- headers/fe-spline_problem_hopk.h
+++ headers/fe-spline_problem_hopk.h
@@ -403,16 +403,11 @@ namespace HMM
 	template <int dim>
 	void FEProblem<dim>::setup_system ()
 	{
-		std::cout << "    Number of degrees of freedom:111 " << std::endl; 
 		dof_handler.distribute_dofs (fe);
-		std::cout << "    Number of degrees of freedom:222 " << std::endl; 
 		locally_owned_dofs = dof_handler.locally_owned_dofs();
-		std::cout << "    Number of degrees of freedom:333 " << std::endl; 
 		DoFTools::extract_locally_relevant_dofs (dof_handler,locally_relevant_dofs);
-		std::cout << "    Number of degrees of freedom:112 " << std::endl; 
 
 		history_dof_handler.distribute_dofs (history_fe);
-		std::cout << "    Number of degrees of freedom:113 " << std::endl; 
 
 		n_local_cells
 		= GridTools::count_cells_with_subdomain_association (triangulation,
@@ -423,7 +418,6 @@ namespace HMM
 		DoFTools::make_hanging_node_constraints (dof_handler,
 				hanging_node_constraints);
 		hanging_node_constraints.close ();
-		std::cout << "    Number of degrees of freedom:1112 " << std::endl; 
 
 		DynamicSparsityPattern sparsity_pattern (locally_relevant_dofs);
 		DoFTools::make_sparsity_pattern (dof_handler, sparsity_pattern,
@@ -432,7 +426,6 @@ namespace HMM
 				local_dofs_per_process,
 				FE_communicator,
 				locally_relevant_dofs);
-		std::cout << "    Number of degrees of freedom:1113 " << std::endl; 
 
 		mass_matrix.reinit (locally_owned_dofs,
 				locally_owned_dofs,
@@ -443,14 +436,12 @@ namespace HMM
 				sparsity_pattern,
 				FE_communicator);
 		system_rhs.reinit (locally_owned_dofs, FE_communicator);
-		std::cout << "    Number of degrees of freedom:1114 " << std::endl; 
 
 		newton_update_displacement.reinit (dof_handler.n_dofs());
 		incremental_displacement.reinit (dof_handler.n_dofs());
 		displacement.reinit (dof_handler.n_dofs());
 		old_displacement.reinit (dof_handler.n_dofs());
 		for (unsigned int i=0; i<dof_handler.n_dofs(); ++i) old_displacement(i) = 0.0;
-		std::cout << "    Number of degrees of freedom:1115 " << std::endl; 
 
 		newton_update_velocity.reinit (dof_handler.n_dofs());
 		incremental_velocity.reinit (dof_handler.n_dofs());
@@ -537,6 +528,8 @@ namespace HMM
 	{
 		// Number of flakes
 		unsigned int nboxes=structure_data.size();
+		dcout << " --------------------- "<<std::endl;
+		dcout << "nboxes "<< nboxes << std::endl;
 
 		// Filling identity matrix
 		Tensor<2,dim> idmat;
